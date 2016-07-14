ddc65efd1103ffc01c48b1c11d079d127cd3e7cd
Joey Vinyard <jviny98@yahoo.com>
2016-07-14 10:35:13 -0400

    Added comments to index.js, and removed debug code

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-
+//Add packages needed to run
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+//Include the index.html file
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 
 });
 
-var clients = [];
+var clients = [];//Initialize a new array to hold all the clients
+
+//Fire whenever a new client connects
 io.on('connection', function(client){
-	console.log("connect");
+	//Transmit to the new client that it was created
 	client.emit('userCreated', clients.length);
-	console.log('CREATING USER WITH ID ' + (clients.length + 1));
+	//Add the new client to the end of the client list
 	clients.push(client.id);
+	//Loop through the client list, telling them there was a new client added
 	for(items in clients){
 		if(clients[items] != client.id){
 			io.sockets.connected[clients[Number(items)]].emit('userAdded', clients.length);
 		}
 	}
+
+	//Fires when the client moves its mouse
 	client.on('mouseMove', function(mousePos){
+		//Loop through the clients and transmit to all of them that a client has moved
 		for(items in clients){
+			//Ignores the client that moved its mouse
 			if(clients[items] != client.id){
 				var idMouse = mousePos;
 				newID = Number((clients.indexOf(client.id) + 1));
@@ -29,19 +37,23 @@ io.on('connection', function(client){
 			}
 		}
 	});
+
+	//Fires when a client disconnects
 	client.on('disconnect', function(){
-		console.log("user left");
 		var id = client.id;
 		var indexOfLeave = clients.indexOf(id);
+		//Removes the disconnected client from the list
 		clients.splice(indexOfLeave, 1)
 		for(items in clients){
 			if(clients[items] != client.id){
+				//Tells each of the remaining clients that somebody left
 				io.sockets.connected[clients[items]].emit('clientLeft', indexOfLeave);
 			}
 		}
 	})
 });
 
+//Opens the port 3001, as weell as the server port
 http.listen(process.env.PORT || 3001, function(){
   console.log('listening on *:3001');
 });
